5f403dc4f623101dedcbb5808c7b92f738a4b8bf
juchen0411 <41322582+juchen0411@users.noreply.github.com>
2020-02-21 09:59:36 +0800

    Update ost-config.php

--- include/ost-config.php
+++ include/ost-config.php
@@ -42,7 +42,7 @@ define('DBTYPE','mysql');
 define('DBHOST','osticketnew.mysql.database.azure.com');
 define('DBNAME','osticketazure');
 define('DBUSER','juc@osticketnew');
-define('DBPASS','!Q');
+define('DBPASS','!Qaz2wsx');
 
 # Table prefix
 define('TABLE_PREFIX','%CONFIG-PREFIX');
